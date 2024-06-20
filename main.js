57833c8d92b533373fee40fccf3ed9646cf56935
pranayadna <pranayadnaap96@gmail.com>
2024-06-20 12:56:17 +0700

    Edit main.js

--- main.js
+++ main.js
@@ -8,17 +8,6 @@ function LinkedList() {
         }
     }
 
-    const getHead = () => head;
-
-    const traverse = () => {
-        let tmp = head;
-        
-        while (tmp !== null) {
-            console.log(tmp.getValue());
-            tmp = tmp.getNextNode();
-        }
-    }
-
     const append = (val) => {
         const newNode = createNode();
         newNode.setValue(val);
@@ -30,11 +19,134 @@ function LinkedList() {
         tmp.setNextNode(newNode);
     }
 
+    const prepend = (val) => {
+        const newNode = createNode();
+
+        newNode.setValue(val);
+        newNode.setNextNode(head);
+        head = newNode;
+    }
+
+    const size = () => {
+        let tmp = head;
+        let total = 0; 
+        
+        while (tmp !== null) {
+            tmp = tmp.getNextNode();
+            total++;
+        }
+
+        return total;
+    }
+    
+    const getHead = () => head;
+    
+    const getTail = () => {
+        let tmp = head;
+        let tail = null; 
+        
+        while (tmp !== null) {
+            tail = tmp;
+            tmp = tmp.getNextNode();
+        }
+
+        return tail;
+    }
+
+    const at = (index) => {
+        let count = 0;
+        let tmp = head;
+        let current = null;
+
+        while (tmp !== null) {
+            if (count === index) {
+                current = tmp;
+            }
+            count++;
+            tmp = tmp.getNextNode();
+        }
+
+        return current;
+    }
+
+    const pop = () => {
+        let length = size();
+        let current = head;
+        let prev = null;
+
+        if (length === 1) {
+            head = null;
+        }
+
+        while (current !== null) {
+            prev = current;
+            prev.setNextNode(null);
+            current = current.getNextNode();
+        }
+    }
+
+    const contains = (value) => {
+        let tmp = head;
+        let result = null;
+
+        while (tmp !== null) {
+            
+            if (tmp.getValue() === value) {
+                result = true;
+                break;
+            } else {
+                result = false;
+            }
+
+            tmp = tmp.getNextNode();
+        }
+
+        return result;
+    }
+
+    const find = (value) => {
+        let tmp = head;
+        let count = 0;
+
+        while (tmp !== null) {
+            
+            if (tmp.getValue() === value) {
+                return count;
+            } else {
+                count++
+                tmp = tmp.getNextNode();
+            }
+
+        }
+
+        return null;
+    }
+
+    const toString = () => {
+        let tmp = head;
+        let str = '';
+
+        while (tmp !== null) {
+            str += `( ${tmp.getValue()} )` + ' -> ';
+            tmp = tmp.getNextNode();
+        }
+        str += null;
+
+        return str;
+    }
+
     return {
         createHeadNode,
+        append,
+        prepend,
+        size,
         getHead,
-        traverse,
-        append
+        getTail,
+        at,
+        pop,
+        contains,
+        find,
+        toString
     }
 }
 
@@ -61,21 +173,15 @@ function createNode() {
     }
 }
 
-const testNode = createNode();
 const linkedList = LinkedList();
-linkedList.createHeadNode(10)
 
-const head = linkedList.getHead();
-const newNode = createNode();
-const newNewNode = createNode();
+linkedList.createHeadNode(20);
+linkedList.append(90);
+linkedList.append(10);
+linkedList.prepend(5);
+linkedList.pop();
 
-newNode.setValue(5);
-head.setNextNode(newNode);
-newNewNode.setValue(20);
-newNode.setNextNode(newNewNode);
-
-console.log(linkedList.append(90));
-console.log(head.getValue());
-console.log(head.getNextNode().getValue());
-console.log(head.getNextNode().getNextNode().getValue());
-console.log(head.getNextNode().getNextNode().getNextNode().getValue());
+console.log(linkedList.at(1).getValue());
+console.log(linkedList.contains(55));
+console.log(linkedList.find(10));
+console.log(linkedList.toString());
